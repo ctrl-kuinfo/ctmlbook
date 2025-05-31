adf0b2a381813f813593918a71ed609f0aef2e2c
kashima-ctrl <kashima@ieee.org>
2025-05-31 18:12:26 +0900

    Figure11_{4,5}.py updated

--- python/Figure11_5.py
+++ python/Figure11_5.py
@@ -1,56 +1,77 @@
 # Author: Kenji Kashima
-# Date  : 2025/05/24
+# Date  : 2025/05/31
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy as sp
+
+# pip install sympy
 
 np.random.seed(33)
 import sys
 sys.path.append("./")
 import config
 
+# 関数 L, L1, L2 の定義（記号式）
+p = sp.Symbol('p')
+
+L1_sym = p**2 / 2 + p / 2
+L2_sym = - p * sp.cos(10 * p) / 20 + sp.sin(10 * p) / 200 - p / 2
+L_sym = ( L1_sym + L2_sym ) / 2 
+
+grad_L_sym  = sp.diff(L_sym, p)
+grad_L1_sym = sp.diff(L1_sym, p)
+grad_L2_sym = sp.diff(L2_sym, p)
+print('grad_L1_sym = ' , grad_L1_sym)
+print('grad_L2_sym = ' , grad_L2_sym)
+
+# 数値計算用に lambdify
+L        = sp.lambdify(p, L_sym, 'numpy')
+grad_L   = sp.lambdify(p, grad_L_sym, 'numpy')
+grad_L1  = sp.lambdify(p, grad_L1_sym, 'numpy')
+grad_L2  = sp.lambdify(p, grad_L2_sym, 'numpy')
 
 def figure11_5a():
     figsize = config.global_config(type= 1)
     plt.figure(figsize=figsize)
-    x = np.arange(-5,5,0.01)
-    y = (x**2/2-x*np.cos(x*10)/20+np.sin(x*10)/20)/2
-    plt.plot(x,y,label=r'$L(x)$')
-    y = (x+x*np.sin(x*10)/2)/2
-    plt.plot(x,y,label=r"$\nabla L(x)$")
-    y = x + 1/2
-    plt.plot(x,y,label=r'$\nabla L_1(x)$')
-    y = x*np.sin(x*10)/2 -1/2
-    plt.plot(x,y,label=r"$\nabla L_2(x)$")
+    p_vals = np.arange(-5,5,0.01)
+    plt.plot(p_vals, L(p_vals), label=r'$L(p)$')
+    plt.plot(p_vals, grad_L(p_vals), label=r'$\nabla L(p)$')
+    plt.plot(p_vals, grad_L1(p_vals), label=r'$\nabla L_1(p)$')
+    plt.plot(p_vals, grad_L2(p_vals), label=r'$\nabla L_2(p)$')
     
     plt.legend()
     plt.grid()
-    plt.xlabel(r"$x$")
+    plt.xlabel(r"$p$")
     plt.tight_layout()
     plt.savefig("./figures/Figure11_5a.pdf")
     plt.show()
 
-def figure11_5b(N_k= 2000):
+def figure11_5b( N_k = 2000):
+    """
+    Runs a SGD simulation to solve L(p)=0.
+
+    Parameters:
+        N_k : simulation time length.
+    """
+        
     figsize = config.global_config(type= 1)
-    C = [1.0, 1.0, 1.0]
-    alpha = [0.4, 0.8, 1.2]
+    C = [1.0, 1.0, 1.0]         # constants for step size
+    alpha = [0.4, 0.8, 1.2]     # decay rate for step size
+    N_setting = len(C)          # Number of settings
 
-    x_list = np.zeros((3,N_k+1))
-    y_list = np.zeros((3,N_k))
+    p_list = np.zeros( ( N_setting , N_k+1 ) )
+    y_list = np.zeros( ( N_setting , N_k ) )
     plt.figure(figsize=figsize)  
-    x_list[:,0] = np.ones_like(x_list[:,0])*1 #start at 2.0
-    for k in range(3):
-        for i in range(N_k):        
-            x = x_list[k,i]
-            randn = np.random.randn()
-            if randn< 0:
-                y_list[k,i] = x + 1/2 ;   # solving x^2 =2
-            else:
-                y_list[k,i] = x*np.sin(x*10)/2 - 1/2 ;   # solving x^2 =2
+    p_ini = 1                   # initial value for SGD
+    p_list[:,0] = np.ones_like(p_list[:,0]) * p_ini 
+    for setting in range( N_setting ):
+        for k in range(N_k):        
+            p = p_list[setting,k]
+            y_list[setting, k] = np.random.choice([grad_L1, grad_L2], p=[0.5, 0.5])(p)
+            p_list[setting,k+1] = p - C[setting]/((k+1)**alpha[setting]) * y_list[setting,k]
+        plt.plot(p_list[setting,:],label=r"$\alpha={}$".format(alpha[setting]))
 
-            x_list[k,i+1] = x - C[k]/((i+1)**alpha[k]) * y_list[k,i]
-        plt.plot(x_list[k,:],label=r"$\alpha={}$".format(alpha[k]))
-
-    plt.scatter(0,1,marker='o',s=40,clip_on=False,color='black',label= "Initial Value",zorder=20)
+    plt.scatter(0,p_ini,marker='o',s=40,clip_on=False,color='black',label= "Initial Value",zorder=20)
     plt.ylabel(r"$p_k$")
     plt.xlabel(r"$k$")
     plt.xlim([0,N_k])
@@ -60,8 +81,6 @@ def figure11_5b(N_k= 2000):
     plt.tight_layout()
     plt.savefig("./figures/Figure11_5b.pdf")
     plt.show()
-
-
 
 if __name__ == '__main__':
     figure11_5a()
